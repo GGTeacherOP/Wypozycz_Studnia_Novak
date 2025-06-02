a22deeae59a14931d873057114ca5d533288647d
loczkek <loczkek2@gmail.com>
2025-06-02 21:50:45 +0200

    Poprawiono przekierwoanie stopki

--- skydrive/admin/includes/admin_footer.php
+++ skydrive/admin/includes/admin_footer.php
@@ -1,4 +1,4 @@
-<a href="/../skydrive/admin/dashboard.php" class="back-to-dashboard">
+<a href='../../admin/dashboard.php' class="back-to-dashboard">
     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
         <path d="M19 12H5M12 19l-7-7 7-7"/>
     </svg>
